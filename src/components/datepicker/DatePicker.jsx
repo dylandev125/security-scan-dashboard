5834697216d7897bb2264adc0247340af7ca77a4
LegendLancer <legendlancer0314@gmail.com>
2021-09-14 07:07:29 -0700

    datepicker-updated

---
+++ src/components/datepicker/DatePicker.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import './DatePicker.scss'
+import Calendar from 'react-calendar';
+import { ReactComponent as DateIcon } from '../../assets/date.svg'
+import { ReactComponent as DropdownIcon } from '../../assets/dropdown-i.svg'
+
+const Button = (props) => {
+  const [visible, setVisible] = useState(false);
+  const [currentDate, onDateChange] = useState(new Date());
+  const handleBlur = (e) => {
+    const currentTarget = e.currentTarget;
+    setTimeout(() => {
+      if (!currentTarget.contains(document.activeElement)) {
+        setVisible(false);
+      }
+    }, 0);
+  };
+
+  return (
+    <div class="custom-datepicker-wrapper">
+
+      <button className="custom-datepicker-btn" onClick={() => { setVisible(!visible) }}>
+        <DateIcon />
+        <span>{currentDate.getFullYear() + '/' + (currentDate.getMonth() + 1) + '/' + (currentDate.getDate())}</span>
+        <DropdownIcon />
+      </button>
+      {
+        visible && <div className="calendar-container" tabIndex={1} onBlur={handleBlur}><Calendar
+            className="custom-calendar"
+            onChange={onDateChange}
+            value={currentDate}
+          /></div>
+      }
+    </div>
+  )
+}
+
+export default Button
--- src/pages/client/Billing/Billing.jsx
+++ src/pages/client/Billing/Billing.jsx
@@ -1,20 +1,16 @@
 import { useState } from 'react'
-import SortableTable from 'react-sortable-table';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import MSortTable from '../../../components/SortTable'
 import Layout from '../../../components/layout/Layout'
 import Panel from '../../../components/panel'
 import Button from '../../../components/button'
+import CustomDatePicker from '../../../components/datepicker'
 import SearchBox from '../../../components/search-box'
-import { ReactComponent as DateIcon } from '../../../assets/date.svg'
-import { ReactComponent as DropdownIcon } from '../../../assets/dropdown-i.svg'
 import { PaidData, UnpaidData } from './data'
+import 'react-calendar/dist/Calendar.css';
 import './Billing.scss'
 
 const Billing = () => {
   const [tab, setTab] = useState(0);
-  const [startDate, setStartDate] = useState(new Date());
 
   const handleClick = (tabIndex) => {
     setTab(tabIndex);
@@ -28,9 +24,7 @@ const Billing = () => {
           <div className="billing-header">
             <div className="button-group">
               <div className="datepicker-wrapper">
-                <DateIcon />
-                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-                <DropdownIcon />
+                <CustomDatePicker label="All dates"/>
               </div>
               <Button label="Paid" handleClick={() => handleClick(0)} />
               <Button label="Unpaid" handleClick={() => handleClick(1)} />
