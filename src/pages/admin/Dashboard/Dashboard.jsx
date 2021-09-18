ac0e4637c669a09f9b3fcbbfe0a570cf70995e9b
LegendLancer <legendlancer0314@gmail.com>
2021-09-18 07:32:37 -0700

    admin-portal-commit-2

--- src/pages/admin/Dashboard/Dashboard.jsx
+++ src/pages/admin/Dashboard/Dashboard.jsx
@@ -1,15 +1,23 @@
 import Layout from '../../../components/layout/Layout'
+import BaseTable from "../../../components/base-table/BaseTable";
+import { TableColumns, Item, SampleData } from "./PageBuilder";
 import './Dashboard.scss'
 
 const Dashboard = () => {
   return (
     <Layout type='dashboard' userType='admin'>
-      <div className="dashboard-wrapper">
-        <div className="box-info">
-          asdffsad
-          <div className="ribon">DASHBOARD</div>
-        </div>
-      </div>
+      <BaseTable
+        className="admin-dashboard-wrapper"
+        hasSearchBox
+        ribon="INVOICE"
+        tableHeader={[{ prefix: 'd' }, { prefix: 'p' }]}
+        tableData={{
+          columns: TableColumns,
+          body: (
+            SampleData.map(element => Item(element))
+          )
+        }}
+      />
     </Layout>
   )
 }
